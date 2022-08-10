caa8b26fc445d8f4216b0c796fdac678a1c6fc17
Patrick Beagan <pbeagan1@gmail.com>
2022-08-09 22:15:22 -0500

    small updates

--- router.py
+++ router.py
@@ -17,7 +17,7 @@ class Router:
         with open("build/index.html", "r") as f:
             return f.read()
 
-    def update_with_name(self, name):
+    def update_with_name(self, name) -> str:
         try:
             with open("build/data.txt", "r") as f:
                 updated_lines = [
@@ -32,14 +32,14 @@ class Router:
                         each.time_last_completed = time.time().__floor__()
 
                 if not matched_at_least_once:
-                    raise Exception("No matches")
+                    return "No matches"
 
                 updated_lines = [
                     it.to_filestring()
                     for it in updated_lines
                 ]
 
-            with open("build/data.txt", "w") as f:
+            with open("build/data.txt", "a") as f:
                 f.write("\n".join(updated_lines))
             return "Success"
         except:
@@ -50,15 +50,20 @@ class Router:
             return f.read()
 
     def add_with_name(self, name, duration):
-        with open("build/data.txt", "a") as f:
-            f.write(Task(name, duration, time.time().__floor__()).to_filestring())
-        return "Success"
+        try:
+            with open("build/data.txt", "a") as f:
+                f.write(Task(name, duration, time.time().__floor__()).to_filestring())
+            return "Success"
+        except Exception as e:
+            return f"Failure {e}"
 
     def help(self):
         return """
+        <html>
         <body>
-        <a href="http://localhost:5000/add/testing/120"><p>Add task</p>
-        <a href="http://localhost:5000/update/testing"><p>Update task</p>
-        <a href="http://localhost:5000/"><p>Check Status</p>
+        <a href="http://localhost:5000/add/testing/120"><p>Add task</p></a>
+        <a href="http://localhost:5000/update/testing"><p>Update task</p></a>
+        <a href="http://localhost:5000/"><p>Check Status</p></a>
         </body>
+        </html>
         """
