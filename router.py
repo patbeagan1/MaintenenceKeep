d9086ffac9e0a40f9560a5eaaf0f7eda37b75aea
Patrick Beagan <pbeagan1@gmail.com>
2022-08-10 00:01:09 -0500

    fully converting to append - works by checking first instance of a task

--- router.py
+++ router.py
@@ -17,13 +17,19 @@ class Router:
 
     def update_with_name(self, name) -> str:
         task_manager = TaskManager()
-        already_exists = any(x.name == name for x in (task_manager.task_list))
 
-        if not already_exists:
+        first = None
+        for x in (task_manager.task_list):
+            if x.name == name:
+                first = x
+                break
+
+        if not first:
             return "Failure, not found"
 
         with open("build/data.csv", "a") as f:
-            f.write("")
+            first.time_last_completed = time.time().__floor__()
+            f.write("\n" + first.to_filestring())
 
         return "Success"
 
